bf30c240b7faecdb50b84abe95f8377f125af447
Araks Arevyan <you@example.com>
2023-08-21 16:55:57 +0400

    edit part

--- index.js
+++ index.js
@@ -8,21 +8,21 @@ const bodyParser = require('body-parser');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(bodyParser.json());
 app.set('view engine', 'ejs')
 
-app.post('/admin/delete-product/:id', function (req, res) {
+
+let arr = [];
+app.post('/admin/delete-product/:id', function (req, res, next) {
 	let id = req.params.id;
-	console.log(id);
 	fs.readFile("info.json", (err, fileContent) => {
 		if (err) {
 			return;
 		}
 		let updatedCart = JSON.parse(fileContent);
-		console.log(updatedCart);
 		updatedCart = updatedCart.filter(
 			prod => prod.id !== id
 		);
-		console.log(updatedCart);
 		fs.writeFile("info.json", JSON.stringify(updatedCart), err => {
 			console.log(err);
 		});
@@ -30,13 +30,51 @@ app.post('/admin/delete-product/:id', function (req, res) {
 	res.redirect('/');
 });
 
+
+app.get('/admin/edit-product/:id', (req, res, next) => {
+	let id = req.params.id;
+	fs.readFile(__dirname + '/info.json', 'utf8', function (err, data) {
+		if (err) {
+			return err;
+		}
+		let editData = JSON.parse(data);
+		let objToEdit = {};
+		for (let obj of editData) {
+			if (obj.id == id)
+				objToEdit = obj;
+		}
+		res.render('edit', {
+			data: objToEdit
+		});
+	});
+});
+
+
+app.post('/admin/edit-product/:id', (req, res, next) => {
+	let id = req.params.id;
+	fs.readFile(__dirname + '/info.json', 'utf8', function (err, data) {
+		if (err) {
+			return err;
+		}
+		let editData = JSON.parse(data);
+
+		for (let obj of editData) {
+			if (obj.id == id) {
+				obj.title = req.body.title;	
+			}
+		}
+		fs.writeFile("info.json", JSON.stringify(editData), function () { });
+	});
+	res.redirect('/');
+});
+
+
 app.get('/', (req, res, next) => {
-	fs.readFile(__dirname + '/info.json', function (err, data) {
+	fs.readFile(__dirname + '/info.json', "utf8", function (err, data) {
 		if (err) {
 			return console.error(err);
 		}
 		let resData = JSON.parse(data);
-		console.log(resData);
 		res.render('test', {
 			resData: resData
 		});
@@ -46,7 +84,6 @@ app.get('/', (req, res, next) => {
 app.post('/admin/add-product', (req, res, next) => {
 	const title = req.body.title;
 	const id = getRandomInt().toString();
-	let arr = [];
 	obj = {
 		title: title,
 		id: id
@@ -56,16 +93,15 @@ app.post('/admin/add-product', (req, res, next) => {
 			arr = JSON.parse(fileContent);
 			arr.push(obj);
 			fs.writeFile("info.json", JSON.stringify(arr), function () { });
-			console.log(arr);
 		}
 	});
 	res.redirect('/');
 });
 
 function getRandomInt(min, max) {
-    min = Math.ceil(10);
-    max = Math.floor(10000);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+	min = Math.ceil(10);
+	max = Math.floor(10000);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 server.listen(3006, () => {
